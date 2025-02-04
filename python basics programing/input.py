872ce872f591a5f62fb3e3648d336bc5d158a637
Bny2004 <115213694+Bny2004@users.noreply.github.com>
2025-02-04 19:34:06 +0530

    Delete python basics programing directory

--- python basics programing/input.py
+++
@@ -1,6 +0,0 @@
-name = input("what is your name?  ")
-print("hello "+ name)
-
-birth_year = input("enter birth year  ")
-age = 2023-int(birth_year)
-print(age)
--- python basics programing/simplecalculator.py
+++
@@ -1,4 +0,0 @@
-first =input("first no:")
-second =input("second no:")
-sum = float(first) + float(second)
-print("sum: "+str(sum))
